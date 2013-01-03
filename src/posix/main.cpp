5d8be00cd07d7f713a87f001170f236c64fd2cd7
Kleber Garcia <kecho_garcia@hotmail.com>
2013-01-02 19:09:17 -0500

    Removing tabs in main

--- src/posix/main.cpp
+++ src/posix/main.cpp
@@ -28,7 +28,7 @@ void * processThread(void * ptr)
         pthread_mutex_lock(&ctx->mutex);
         ctx->program->Render();
         cairo_set_source_surface(ctx->cairoContext,ctx->surface,0,0); 
-		cairo_paint(ctx->cairoContext);
+        cairo_paint(ctx->cairoContext);
         pthread_mutex_unlock(&ctx->mutex);
     }
     return 0;
@@ -45,60 +45,60 @@ int main (int argc, char *argv[])
     pthread_t processThreadHandle;
     ProgramInterface * p = ProgramInterface::create();
     p->OnStart();
-	Display *dpy;
-	Window rootwin;
-	Window win;
-	XEvent e;
-	int scr;
-	cairo_surface_t *cs;
+    Display *dpy;
+    Window rootwin;
+    Window win;
+    XEvent e;
+    int scr;
+    cairo_surface_t *cs;
 
-	if(!(dpy=XOpenDisplay(NULL))) {
-		fprintf(stderr, "ERROR: Could not open display\n");
-		exit(1);
-	}
+    if(!(dpy=XOpenDisplay(NULL))) {
+        fprintf(stderr, "ERROR: Could not open display\n");
+        exit(1);
+    }
 
-	scr=DefaultScreen(dpy);
-	rootwin=RootWindow(dpy, scr);
+    scr=DefaultScreen(dpy);
+    rootwin=RootWindow(dpy, scr);
 
-	win=XCreateSimpleWindow(dpy, rootwin, 1, 1, p->GetFrame()->GetW(), p->GetFrame()->GetH(), 0, BlackPixel(dpy, scr), BlackPixel(dpy, scr));
+    win=XCreateSimpleWindow(dpy, rootwin, 1, 1, p->GetFrame()->GetW(), p->GetFrame()->GetH(), 0, BlackPixel(dpy, scr), BlackPixel(dpy, scr));
 
-	XStoreName(dpy, win, p->GetName());
-	XSelectInput(dpy, win, ExposureMask|ButtonPressMask|CWEventMask);
-	XMapWindow(dpy, win);
+    XStoreName(dpy, win, p->GetName());
+    XSelectInput(dpy, win, ExposureMask|ButtonPressMask|CWEventMask);
+    XMapWindow(dpy, win);
 
     cs=cairo_xlib_surface_create(dpy, win, DefaultVisual(dpy, 0), p->GetFrame()->GetW(), p->GetFrame()->GetH());
 
-	Atom wmDelete=XInternAtom(dpy, "WM_DELETE_WINDOW", True);
-	XSetWMProtocols(dpy, win, &wmDelete, 1);
+    Atom wmDelete=XInternAtom(dpy, "WM_DELETE_WINDOW", True);
+    XSetWMProtocols(dpy, win, &wmDelete, 1);
     cairo_t *cr = cairo_create (cs);
-	cairo_surface_t * imageSurface = cairo_image_surface_create_for_data(
+    cairo_surface_t * imageSurface = cairo_image_surface_create_for_data(
             reinterpret_cast<unsigned char *>(p->GetFrame()->GetBuffer()), CAIRO_FORMAT_ARGB32, p->GetFrame()->GetW() , p->GetFrame()->GetH(), cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, p->GetFrame()->GetW()));
     render_context renderContext; 
     renderContext.surface = imageSurface;
     renderContext.cairoContext = cr;
     renderContext.program = p;
     pthread_mutex_init(&renderContext.mutex, NULL);
-	bool started = false;	
-	while(!requestExit) {
-		XNextEvent(dpy, &e);
+    bool started = false;    
+    while(!requestExit) {
+        XNextEvent(dpy, &e);
         pthread_mutex_lock(&renderContext.mutex);
-		if(e.type==Expose && e.xexpose.count<1) {
+        if(e.type==Expose && e.xexpose.count<1) {
             if (!started) {
                 start_process_thread(&processThreadHandle, &renderContext);
                 started = true;
             }
-		} else if(e.type==ButtonPress) {
+        } else if(e.type==ButtonPress) {
 
         } else if (e.type == ClientMessage) {
             requestExit = true;
-		}
+        }
         pthread_mutex_unlock(&renderContext.mutex);
-	}
+    }
     pthread_join(processThreadHandle, NULL);
     pthread_mutex_destroy(&renderContext.mutex);
     cairo_surface_destroy(imageSurface);
-	cairo_surface_destroy(cs);
-	XCloseDisplay(dpy);
+    cairo_surface_destroy(cs);
+    XCloseDisplay(dpy);
     p->OnEnd();
     delete p;
     return 0;
